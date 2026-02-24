cd85bc8d92450e09d489a861314f41e354b6df1e
MadaIsaac <madaisaac2002@gmail.com>
2026-02-24 19:29:59 +0200

    Update school_score.py

--- school_score.py
+++ school_score.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+
 
 whls_items = {
     "Wanga snacks": 7800,
@@ -9,3 +11,9 @@ st.write("mitengo ya katundu yemwe ali pa msika panopa")
 
 for item, price in whls_items.items():
     st.write(f"nice price{item}")
+
+df = pd.DataFrame(list(whls_items.items()),columns=["item","price(mk)"])
+df["price(mk)" ].df["price(mk)"].apply(lambda x: f"mk{x:,}") 
+
+st.table(df)
+                   
