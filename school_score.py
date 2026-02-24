f17e056b6ac028f34d9cdc652bc84a77827d028c
MadaIsaac <madaisaac2002@gmail.com>
2026-02-24 19:17:36 +0200

    te

--- school_score.py
+++ school_score.py
@@ -3,9 +3,9 @@ import streamlit as st
 whls_items = {
     "Wanga snacks": 7800,
     "balst snacks":7800,
-    "tiger pipe":4000}
+    "tiger pipe":4000,}
 
 st.write("mitengo ya katundu yemwe ali pa msika panopa")
 
-whls_items.items():
-st.write(f"take this {item} tengo wake {price:,}")
+for item, price in whls_items.items():
+    st.write(f"nice price{item}")
